219ad5dddb5ae718687487813847f292eb9d18c6
Roshan Sharma <reprogram46@gmail.com>
2021-02-10 17:15:32 +0530

    Initial commit
    Create an empty CLI application.

---
+++ cmd/root.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command
+var rootCmd = &cobra.Command{
+	Use:   "uncrypt",
+	Short: "Encrypt and decrypt tagged files.",
+	Long:  `uncrypt helps you tag files and encrypt/decrypt them via a subcommand.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Ah shit here we go again")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.uncrypt.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".uncrypt" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".uncrypt")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
