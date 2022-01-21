6f262761046477c9db8566543b9dfe210d97d1d4
Roshan Sharma <reprogram46@gmail.com>
2022-01-22 02:27:52 +0530

    chore: Setup routing and app layout

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,3 +1,4 @@
+import { ContentLayout, Layout } from '@strapi/design-system/Layout'
 import {
   MainNav,
   NavLink,
@@ -14,40 +15,47 @@ const Home = () => {
   return <h1>Welcome to uCrypt</h1>
 }
 
+const DrawerNav = () => (
+  <MainNav condensed={false}>
+    <NavSections>
+      <NavSection label="Actions">
+        <NavLink to="/encrypt" icon={<Lock />}>
+          Encrypt
+        </NavLink>
+        <NavLink to="/decrypt" icon={<Key />}>
+          Decrypt files
+        </NavLink>
+        <NavLink to="/files" icon={<BulletList />}>
+          All files
+        </NavLink>
+        <NavLink to="/search" icon={<Search />}>
+          Search
+        </NavLink>
+      </NavSection>
+      <NavSection label="Settings">
+        <NavLink to="/change-key">Change key</NavLink>
+        <NavLink to="/reset">Backup and reset</NavLink>
+      </NavSection>
+      <NavSection label="Support">
+        <NavLink to="/theory">How it works</NavLink>
+        <NavLink to="/privacy">Privacy Policy</NavLink>
+      </NavSection>
+    </NavSections>
+  </MainNav>
+)
+
 export default function App() {
   return (
     <ThemeProvider theme={lightTheme}>
-      <AppHeader />
       <Router>
-        <MainNav condensed={false}>
-          <NavSections>
-            <NavSection label="Actions">
-              <NavLink to="/encrypt" icon={<Lock />}>
-                Encrypt
-              </NavLink>
-              <NavLink to="/decrypt" icon={<Key />}>
-                Decrypt files
-              </NavLink>
-              <NavLink to="/list" icon={<BulletList />}>
-                All files
-              </NavLink>
-              <NavLink to="/search" icon={<Search />}>
-                Search
-              </NavLink>
-            </NavSection>
-            <NavSection label="Settings">
-              <NavLink to="/change-key">Change key</NavLink>
-              <NavLink to="/reset">Backup and reset</NavLink>
-            </NavSection>
-            <NavSection label="Support">
-              <NavLink to="/theory">How it works</NavLink>
-              <NavLink to="/privacy">Privacy Policy</NavLink>
-            </NavSection>
-          </NavSections>
-        </MainNav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <AppHeader />
+        <Layout sideNav={<DrawerNav />}>
+          <ContentLayout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </ContentLayout>
+        </Layout>
       </Router>
     </ThemeProvider>
   )
