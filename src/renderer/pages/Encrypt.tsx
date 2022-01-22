55368f0f64397438be8bbf887c6dd49c505ef634
Roshan Sharma <reprogram46@gmail.com>
2022-01-22 23:59:11 +0530

    feat: Confirm before proceeding with dangerous mode

--- src/renderer/pages/Encrypt.tsx
+++ src/renderer/pages/Encrypt.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@strapi/design-system/Box'
 import { Button } from '@strapi/design-system/Button'
 import { Checkbox } from '@strapi/design-system/Checkbox'
+import { Dialog, DialogBody, DialogFooter } from '@strapi/design-system/Dialog'
 import { Divider } from '@strapi/design-system/Divider'
 import { Flex } from '@strapi/design-system/Flex'
 import { GridLayout } from '@strapi/design-system/Layout'
+import { Stack } from '@strapi/design-system/Stack'
 import { Typography } from '@strapi/design-system/Typography'
-import { Lock, Plus } from '@strapi/icons'
+import { ExclamationMarkCircle, Lock, Plus } from '@strapi/icons'
 import { ipcRenderer } from 'electron'
 import path from 'path'
 import { useState } from 'react'
@@ -14,10 +16,26 @@ import FileListItem from '../components/FileListItem'
 export default function EncryptScreen() {
   const [files, setFiles] = useState<string[]>([])
   const [checked, setChecked] = useState<boolean>(true)
+  const [alertDialogShown, setAlertDialogShown] = useState<boolean>(false)
+  const [continueDangerously, setContinueDangerously] = useState<boolean>(false)
 
   async function selectFiles() {
     const filePaths = await ipcRenderer.invoke('OPEN_FILE_SELECT')
     setFiles(filePaths)
+  }
+
+  async function beginEncryption() {
+    if (!checked && !continueDangerously) {
+      setAlertDialogShown(true)
+      // eslint-disable-next-line no-useless-return
+      return
+    }
+    // TODO: Continue with encryption based on value of `checked`
+  }
+
+  const finalizeSettings = () => {
+    setAlertDialogShown(false)
+    setContinueDangerously(true)
   }
 
   const EncryptionFlow = () => (
@@ -39,8 +57,50 @@ export default function EncryptScreen() {
           Preserve original files
         </Checkbox>
       </Box>
+
+      {alertDialogShown && (
+        <Dialog
+          title="Hold up! Would you really like to continue?"
+          isOpen={alertDialogShown}
+        >
+          <DialogBody icon={<ExclamationMarkCircle />}>
+            <Stack size={2}>
+              <Flex justifyContent="center">
+                <Typography>
+                  You have opted to not preserve the original files. Doing so
+                  will replace the original file with its encrypted version,
+                  which won&apos;t be accessible without the key. Please proceed
+                  with caution.
+                </Typography>
+              </Flex>
+            </Stack>
+          </DialogBody>
+          <DialogFooter
+            startAction={
+              <Button
+                onClick={() => setAlertDialogShown(false)}
+                variant="tertiary"
+              >
+                Cancel
+              </Button>
+            }
+            endAction={
+              <Button onClick={finalizeSettings} variant="danger-light">
+                Confirm
+              </Button>
+            }
+          />
+        </Dialog>
+      )}
+
       <Box>
-        <Button startIcon={<Lock />} size="L" variant="primary" fullWidth>
+        <Button
+          onClick={beginEncryption}
+          startIcon={<Lock />}
+          size="L"
+          variant={checked ? 'primary' : 'danger-light'}
+          fullWidth
+        >
           ENCRYPT
         </Button>
       </Box>
